8f15d4cd6afb58055c53e64005c62fb698bb1ab5
brocha05 <brocha_05@hotmail.com>
2023-02-13 23:08:57 -0600

    Reading params

--- src/heroes/pages/Hero.jsx
+++ src/heroes/pages/Hero.jsx
@@ -1,9 +1,15 @@
-import { useParams } from "react-router-dom"
+import { Navigate, useParams } from "react-router-dom"
+import { getHeroById } from "../helpers/getHeroById";
 
 export const Hero = () => {
 
-  const params = useParams();
-  console.log(params)
+  const {id} = useParams();
+  const hero = getHeroById(id);
+  
+  if(!hero){
+    return <Navigate to="/marvel"/>
+  }
+
   return (
     <div>Hero</div>
   )
