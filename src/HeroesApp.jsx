6c86974db5161e7ab7f690a92cd58174d0a9e209
Brocha05 <brocha_05@hotmail.com>
2023-03-24 08:27:15 -0600

    Creating context and reducer

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,9 +1,10 @@
 import { AppRoutes } from "./routes/AppRoutes"
 import './assets/styles.css'
+import { AuthProvider } from "./auth/context/AuthProvider"
 export const HeroesApp = () => {
   return (
-    <>
+    <AuthProvider>
       <AppRoutes/>
-    </>
+    </AuthProvider>
   )
 }
